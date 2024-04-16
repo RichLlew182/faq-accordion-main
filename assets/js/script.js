a01c5bf3048d1a46c7ead7029cbd729fd2ce93c6
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-16 16:06:39 +0100

    changed functionality to a function that is added inline to the faq elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 console.log('Hello!')
 
-  const faqItems = document.querySelectorAll('.faq');
+function faqClick(clickedQuestion) {
 
-  faqItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      item.classList.toggle('open');
-    });
-  });
+  clickedQuestion.classList.toggle('open');
+
+  const icon = clickedQuestion.querySelector('img');
+
+  if (clickedQuestion.classList.contains('open')) {
+    icon.src = 'assets/images/icon-minus.svg'
+  }
+
+  else {
+    icon.src ='assets/images/icon-plus.svg'
+  }
+    
+  }
+
