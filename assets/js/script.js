d78ad51e5d8ce9adb1feb448165ec36839909628
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-16 16:50:03 +0100

    added a container class to set the widths on different devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@ function faqClick(e) {
   e.preventDefault()
   const clickedQuestion = e.currentTarget;
 
-  clickedQuestion.classList.toggle('open');
+  clickedQuestion.classList.toggle('active');
 
   const icon = clickedQuestion.querySelector('img');
 
-  if (clickedQuestion.classList.contains('open')) {
-    icon.src = 'assets/images/icon-minus.svg';
+  if (clickedQuestion.classList.contains('active')) {
+    icon.src = './assets/images/icon-minus.svg';
     icon.alt= "Close Icon"
   }
 
   else {
-    icon.src = 'assets/images/icon-plus.svg';
+    icon.src = './assets/images/icon-plus.svg';
     icon.alt= "Open Icon"
   }
     
