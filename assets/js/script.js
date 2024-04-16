7561a43e2a6d8e11e29944a7cf46ade0c7ea0eba
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-16 16:17:24 +0100

    changed the function so that it is not written inline in the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-console.log('Hello!')
+function faqClick(e) {
 
-function faqClick(clickedQuestion) {
+  e.preventDefault()
+  const clickedQuestion = e.currentTarget;
 
   clickedQuestion.classList.toggle('open');
 
   const icon = clickedQuestion.querySelector('img');
 
   if (clickedQuestion.classList.contains('open')) {
-    icon.src = 'assets/images/icon-minus.svg'
+    icon.src = 'assets/images/icon-minus.svg';
+    icon.alt= "Close Icon"
   }
 
   else {
-    icon.src ='assets/images/icon-plus.svg'
+    icon.src = 'assets/images/icon-plus.svg';
+    icon.alt= "Open Icon"
   }
     
-  }
+}
+
+const faqQuestions = document.querySelectorAll('.faq');
+  
+faqQuestions.forEach((question) => {
+  question.addEventListener('click', faqClick);
+});
 
